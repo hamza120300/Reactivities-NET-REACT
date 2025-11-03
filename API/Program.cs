9492ebff6986162fb5c256145f48324ae60373c3
amabdallah@ejada.com <amabdallah@ejada.com>
2025-11-03 13:05:26 +0200

    create data base

--- API/Program.cs
+++ API/Program.cs
@@ -16,4 +16,24 @@ var app = builder.Build();
 
 app.MapControllers();
 
-app.Run();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDate(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+
+}
+
+app.Run(); 
+
+// DbInitializer  
+
